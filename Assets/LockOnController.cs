ff3adf8ea7509684d472468f227d7fd3ce751127
YoshiTa0527 <dfingao0527@gmail.com>
2021-03-01 20:23:30 +0900

    FreeCameraを設定した。ProjectSettingでパッドでカメラ操作をできるようにした。 ・デッドゾーンと感度を調整してスティックに触らなくてもカメラが勝手に動いてしまう問題を解決した

---
+++ Assets/LockOnController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 範囲内で、最も近い敵をロックオンする   
+/// </summary>
+public class LockOnController : MonoBehaviour
+{
+    [SerializeField] float m_searchRange = 4f;
+    [SerializeField] float m_lockonInterval = 1f;
+    GameObject m_target = null;
+    float m_timer;
+
+    public GameObject GetTarget
+    {
+        get { return m_target; }
+    }
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+
+        if (m_timer > m_lockonInterval)
+        {
+            m_timer = 0;
+
+            GameObject[] enemyArray = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach (var enemy in enemyArray)
+            {
+                float dis = Vector3.Distance(this.transform.position, enemy.transform.position);
+
+                if (dis < m_searchRange)
+                {
+                    if (m_target == null || dis < Vector3.Distance(this.transform.position, m_target.transform.position))
+                    {
+                        m_target = enemy;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// ターゲット送り。現在ターゲットしている敵の次の敵をロックオンする
+    /// </summary>
+    public GameObject GetNextTarget
+    {
+        get { return m_target; }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの動きを決める。
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_movingSpeed = 5f;
+    [SerializeField] float m_turnSpeed = 5f;
+    [SerializeField] float m_jumpPower = 5f;
+    [SerializeField] float m_sphereRadius = 1f;
+    [SerializeField] float m_rayMaxDistance = 1f;
+    [SerializeField] LayerMask m_groundMask;
+    Rigidbody m_rb;
+
+    private void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+
+        //// カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+        dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+        dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+
+        if (IsGround())
+        {
+            if (dir == Vector3.zero)
+            {
+                Debug.Log(dir.ToString());
+                m_rb.velocity = new Vector3(0f, m_rb.velocity.y, 0f);
+            }
+            else
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(dir);
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);  // Slerp を使うのがポイント
+
+                Vector3 velo = dir.normalized * m_movingSpeed; // 入力した方向に移動する
+                velo.y = m_rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+                m_rb.velocity = velo;   // 計算した速度ベクトルをセットする
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_rb.AddForce(Vector3.up * m_jumpPower, ForceMode.Impulse);
+
+            }
+        }
+
+    }
+
+    RaycastHit m_hit;
+    bool IsGround()
+    {
+        Ray ray = new Ray(this.transform.position, Vector3.down);
+
+        bool isGround = Physics.SphereCast(ray, m_sphereRadius, out m_hit, m_rayMaxDistance, m_groundMask);
+
+
+        return isGround;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 start = this.transform.position;   // start: オブジェクトの中心
+        Vector3 end = start + Vector3.down * m_rayMaxDistance;  // end: start から真下の地点
+
+
+        Gizmos.DrawSphere(end, m_sphereRadius);
+    }
+
+
+
+}
