155a75cd0838e4e9240ba60b09d82de3feb4365e
YoshiTa0527 <dfingao0527@gmail.com>
2021-03-03 08:18:29 +0900

    ロック送り機能を追加した。調整が必要

--- Assets/Script/LockOnController.cs
+++ Assets/Script/LockOnController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 
 /// <summary>
-/// 敵をロックオンする
+/// 敵をロックオンする。3/3ロック送り機能を追加することとm_targetがnullの時の対処
 /// </summary>
 public class LockOnController : MonoBehaviour
 {
@@ -14,12 +14,15 @@ public class LockOnController : MonoBehaviour
     [SerializeField] float m_lockOnRange = 5f;
     /// <summary>現在ターゲット可能な敵のリスト</summary>
     List<EnemyTargetController> m_targets = new List<EnemyTargetController>();
+    List<EnemyTargetController> m_orderedTargets = new List<EnemyTargetController>();
     EnemyTargetController m_target;
+    EnemyTargetController m_currentTarget;
     public GameObject GetTarget { get { return m_target.gameObject; } }
     GameObject m_enemyParent;
     public static bool IsLock { get; set; }
     GameObject player;
-
+    int m_targetIndex = 0;
+    float m_timer = 0;
 
     private void Start()
     {
@@ -41,7 +44,7 @@ public class LockOnController : MonoBehaviour
             Debug.Log("target lost.");
         }
 
-        /*スティック押し込みで敵をロックオンする*/
+
 
         /*とりあえず敵を全て取得する*/
         EnemyTargetController[] targets = m_enemyParent.transform.GetComponentsInChildren<EnemyTargetController>();
@@ -56,35 +59,52 @@ public class LockOnController : MonoBehaviour
             }
         }
 
-        /*一番近い敵をターゲットする*/
-        if (!m_target && m_targets.Count > 0)
-        {
-            m_target = m_targets.OrderBy(t => Vector3.Distance(player.transform.position, t.transform.position)).First();
-        }
-
         /*もしロックオン状態だったら、ターゲットの位置にロックオンカーソルを表示し続ける*/
         if (IsLock && m_lockOnMarkerImage && m_target)
         {
+
             m_lockOnMarkerImage.rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, m_target.transform.position);
         }
         else { UnLockEnemy(); }
 
-        if (Input.GetButtonDown("PadStickPush"))
+        if (IsLock && m_orderedTargets.Count >= 0)
         {
+            /*複数の敵がロックオン可能な場合十字キー左右で敵の選択*/
+            float horizon = Input.GetAxis("D-Pad H");
+            if (horizon > 0 || horizon < 0)
+            {
+                /*感度が良すぎるので少し待ってからターゲットを切り替えたい*/
+                m_timer += Time.deltaTime;
+                if (m_timer > 0.1f)
+                {
+                    m_timer = 0;
+                    m_targetIndex += (int)horizon;
+                    Debug.Log($"Input::十字キー左右が押された。{m_targetIndex.ToString()}");
+                    m_targetIndex = (m_targetIndex + 1) % m_orderedTargets.Count;
+
+                }
+                m_target = m_orderedTargets[m_targetIndex];
+            }
+        }
+
+        /*スティック押し込みで敵をロックオンする*/
+        if (Input.GetButtonDown("PadStickPush") && m_targets.Count > 0)
+        {
+            m_targetIndex = 0;
             Debug.Log("スティック押し込み");
             if (!IsLock)
             {
 
+
+                DetectNearestTarget();
+                m_target = m_orderedTargets[m_targetIndex];
                 LockOnEnemy();
-
-
             }
             else
             {
                 UnLockEnemy();
             }
         }
-
     }
     /// <summary>
     /// 敵をロックオンする
@@ -93,8 +113,6 @@ public class LockOnController : MonoBehaviour
     {
         Debug.Log("Lock on Enemy");
         IsLock = true;
-
-
     }
     /// <summary>
     /// 敵のロックオンを外す
@@ -126,6 +144,16 @@ public class LockOnController : MonoBehaviour
         {
             m_lockOnMarkerImage.rectTransform.position = -1 * m_lockOnMarkerImage.rectTransform.rect.size;
         }
+    }
+
+    /// <summary>
+    /// 敵を近い順に振り分けたリストを返す
+    /// </summary>
+    /// <returns></returns>
+    void DetectNearestTarget()
+    {
+        m_orderedTargets = m_targets.OrderBy(t => Vector3.Distance(player.transform.position, t.transform.position)).ToList();
+
     }
 
 }
---
+++ Assets/Script/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> intlist = new List<int> { 1, 5, 4, 3 };
+        List<int> orderedList = intlist.OrderBy(i => i).ToList();
+        orderedList.ForEach(i => Debug.Log(i.ToString()));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
