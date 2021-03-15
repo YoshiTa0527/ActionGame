a767aee1a48c1c2b82e2a4058448c137bc4c363b
YoshiTa0527 <dfingao0527@gmail.com>
2021-03-15 19:42:30 +0900

    configurableJointを使って敵を引き寄せる機能を追加した。

---
+++ Assets/Script/GrapplingManager.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GrapplingManager : MonoBehaviour
+{
+
+    /// <summary>ボタンを押すと、jointの制限距離を短くする </summary>
+    [SerializeField] float m_limit = 1f;
+    /// <summary>プレイヤーとターゲットの距離がこの距離以下になるとターゲットは減速する</summary>
+    [SerializeField] float m_disToDecelerateTarget = 2f;
+    /// <summary>ターゲットを減速させるときにかける数</summary>
+    [SerializeField] float m_decelerateSpeed = 0.5f;
+
+    Rigidbody m_targetRb;
+    ConfigurableJoint m_joint;
+    LockOnController m_lockOn;
+    LineRenderer m_lineRend;
+
+    public static bool IsHooked { get; set; }
+
+    private void Start()
+    {
+        m_lineRend = GetComponent<LineRenderer>();
+        m_joint = GetComponent<ConfigurableJoint>();
+        m_lockOn = FindObjectOfType<LockOnController>();
+    }
+
+    private void Update()
+    {
+        /*ロックオン中ならターゲットにフックをつける*/
+        if (LockOnController.IsLock)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!IsHooked)
+                {
+                    IsHooked = true;
+                    Hook(m_joint, m_lockOn.GetTarget);
+                }
+                else IsHooked = false;
+            }
+
+        }
+        else
+        {
+            IsHooked = false;
+        }
+
+        if (IsHooked)
+        {
+            DrawLine(this.transform.position, m_lockOn.GetTarget.transform.position);
+            if (Input.GetButtonDown("Fire2"))
+            {
+                PullTarget(m_joint, m_lockOn.GetTarget);
+                Debug.Log($"pushed Fire2::{m_joint.linearLimit.limit}");
+            }
+
+            if (Vector3.Distance(this.transform.position, m_lockOn.GetTarget.transform.position) <= m_disToDecelerateTarget)
+            {
+                m_targetRb.velocity = Vector3.zero;
+                IsHooked = false;
+            }
+
+        }
+        else
+        {
+            LoseHook(m_joint);
+            HideLine();
+        }
+
+
+    }
+
+    void Hook(ConfigurableJoint joint, GameObject target)
+    {
+        m_targetRb = target.GetComponent<Rigidbody>();
+        if (m_targetRb)
+        {
+            joint.connectedBody = m_targetRb;
+
+            var cj = joint.linearLimit;
+            cj.limit = Vector3.Distance(this.transform.position, target.transform.position);
+            joint.linearLimit = cj;
+
+            joint.xMotion = ConfigurableJointMotion.Limited;
+            joint.yMotion = ConfigurableJointMotion.Limited;
+            joint.zMotion = ConfigurableJointMotion.Limited;
+
+
+
+        }
+        else
+        {
+            Debug.LogErrorFormat("{0} doesn't have Rigidbody.", target.name);
+        }
+    }
+
+    void LoseHook(ConfigurableJoint joint)
+    {
+        joint.connectedBody = null;
+        joint.xMotion = ConfigurableJointMotion.Free;
+        joint.yMotion = ConfigurableJointMotion.Free;
+        joint.zMotion = ConfigurableJointMotion.Free;
+    }
+
+    void PullTarget(ConfigurableJoint joint, GameObject target)
+    {
+        var cj = joint.linearLimit;
+        cj.limit = 2f;
+        joint.linearLimit = cj;
+
+    }
+
+    void DrawLine(Vector3 source, Vector3 destination)
+    {
+        m_lineRend.SetPosition(0, source);
+        m_lineRend.SetPosition(1, destination);
+    }
+
+    void HideLine()
+    {
+        DrawLine(Vector3.zero, Vector3.zero);
+    }
+}
---
+++ Assets/Script/GrapplingManagerVer2.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingManagerVer2 : MonoBehaviour
+{
+
+    /// <summary>ボタンを押すと、jointの制限距離を短くする </summary>
+    [SerializeField] Transform m_pullPoint;
+
+    SpringJoint m_joint;
+    LockOnController m_lockOn;
+    LineRenderer m_lineRend;
+    private bool m_isGrappling = false;
+
+    private void Start()
+    {
+        m_lineRend = GetComponent<LineRenderer>();
+        m_joint = GetComponent<SpringJoint>();
+        m_lockOn = FindObjectOfType<LockOnController>();
+    }
+    private void Update()
+    {
+        /*ロックオン中ならターゲットにフックをつける*/
+        if (LockOnController.IsLock)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!m_isGrappling)
+                {
+                    m_isGrappling = true;
+                }
+                else m_isGrappling = false;
+            }
+
+        }
+        else
+        {
+            m_isGrappling = false;
+        }
+
+        if (m_isGrappling)
+        {
+            if (Input.GetButtonDown("Fire2"))
+            {
+               
+                Debug.Log("pushed Fire2");
+            }
+            DrawLine(this.transform.position, m_lockOn.GetTarget.transform.position);
+
+        }
+        else
+        {
+           
+            HideLine();
+        }
+
+
+    }
+
+    void Hook()
+    {
+
+    }
+
+    void LoseHook()
+    { }
+
+
+    void PullTarget()
+    {
+
+    }
+
+    void DrawLine(Vector3 source, Vector3 destination)
+    {
+        m_lineRend.SetPosition(0, source);
+        m_lineRend.SetPosition(1, destination);
+    }
+
+    void HideLine()
+    {
+        DrawLine(Vector3.zero, Vector3.zero);
+    }
+
+}
